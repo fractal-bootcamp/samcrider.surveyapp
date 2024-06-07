2f8823a9dfe106a3bd9fcf01c3318c117d623685
SamuelRCrider <sam.crider@aol.com>
2024-06-07 19:02:59 -0400

    partial take survey page

--- app/components/pages/Create/component.tsx
+++ app/components/pages/Create/component.tsx
@@ -3,25 +3,6 @@ import { Fields } from "./types";
 
 const Component = ({ onClick, questions }: Fields) => {
   return (
-    // <div>
-    //   <h1>Create a survey</h1>
-    //   <a href="/">go home</a>
-
-    //   <Form method="post">
-    //     <label>name</label>
-    //     <input type="text" name="title" />
-    //     <button type="button" onClick={onClick}>
-    //       add question
-    //     </button>
-    //     {questions.map((q, i) => (
-    //       <div key={i}>
-    //         <label>Question {i + 1}</label>
-    //         <input type={q.type} name={q.name} />
-    //       </div>
-    //     ))}
-    //     <button type="submit">create</button>
-    //   </Form>
-    // </div>
     <div className="flex flex-col items-center gap-10 p-10 h-[100%]">
       <div className="text-2xl font-bold uppercase w-[100%]">Total Survey</div>
       <div className="flex flex-row justify-evenly">
--- app/components/pages/Survey/component.tsx
+++ app/components/pages/Survey/component.tsx
@@ -3,15 +3,46 @@ import { Fields } from "./types";
 
 const Component = ({ survey }: Fields) => {
   return (
-    <div>
-      <h1>{survey.title}</h1>
-      <a href="/">Go home</a>
+    // <div>
+    //   <h1>{survey.title}</h1>
+    //   <a href="/">Go home</a>
+    //   <Form method="post">
+    //     {survey.questions.map((q) => (
+    //       <div key={q.id}>
+    //         <label>{q.question}</label>
+    //         <input type="text" name={q.question} />
+    //       </div>
+    //     ))}
+    //     <button type="submit">submit</button>
+    //   </Form>
+    // </div>
+    <div className="flex flex-col items-center gap-10 p-10 h-[100%]">
+      <div className="text-2xl font-bold uppercase w-[100%]">Total Survey</div>
+      <div className="flex flex-row justify-evenly">
+        <div className="w-[50%]">
+          Do you like surveys? If you do, you've come to the right place.
+          Wondering what to do now that you're here? Take or Create baby!
+        </div>
+        <a className="btn btn-secondary" href="/">
+          Go Home
+        </a>
+      </div>
+      <div className="divider divider-start mx-10 mt-10 uppercase">
+        {survey.title}
+      </div>
       <Form method="post">
         {survey.questions.map((q) => (
-          <div key={q.id}>
-            <label>{q.question}</label>
-            <input type="text" name={q.question} />
-          </div>
+          <label key={q.id} className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">{q.question}</span>
+            </div>
+            <input
+              type="text"
+              name={q.question}
+              className="input input-bordered input-gray w-full max-w-xs"
+              placeholder="Type here"
+            />
+          </label>
         ))}
         <button type="submit">submit</button>
       </Form>
